e3ec86a7a1379afb3a6e1db291f09e60384d90a1
gmcln-rct <producersama@gmail.com>
2021-02-17 18:30:09 -0500

    Add initial animals

---
+++ src/store/modules/animals.js
@@ -0,0 +1,54 @@
+export default {
+  namespaced: true,
+  state() {
+    return {
+      animals: [
+        {
+          id: 'animal001',
+          commonName: 'African wild dog',
+          scientificName: 'Lycaon pictus',
+          speed: '45',
+          siteLink: 'https://en.wikipedia.org/wiki/African_wild_dog',
+          img: 'https://faster-than.s3.amazonaws.com/animal-wilddog.png'
+        },
+        {
+          id: 'animal002',
+          commonName: "Anna's hummingbird",
+          scientificName: 'Calypte anna',
+          speed: '61.06',
+          siteLink: 'https://en.wikipedia.org/wiki/Anna%27s_hummingbird',
+          img: 'https://faster-than.s3.amazonaws.com/animal-annahummingbird.png'
+        },
+        {
+          id: 'animal003',
+          commonName: 'Atlantic bluefin tuna',
+          scientificName: 'Thunnus thynnus',
+          speed: '43.51',
+          siteLink: 'https://en.wikipedia.org/wiki/Atlantic_bluefin_tuna',
+          img: 'https://faster-than.s3.amazonaws.com/animal-bluefin.png'
+        },
+        {
+          id: 'animal004',
+          commonName: 'Bison',
+          scientificName: 'Bison',
+          speed: '35',
+          siteLink: 'https://en.wikipedia.org/wiki/Bison',
+          img: 'https://faster-than.s3.amazonaws.com/animal-annahummingbird.png'
+        },
+        {
+          id: 'animal005',
+          commonName: 'Black mamba snake',
+          scientificName: 'Dendroaspis polylepis',
+          speed: '12.9',
+          siteLink: 'https://en.wikipedia.org/wiki/Black_mamba',
+          img: 'https://faster-than.s3.amazonaws.com/animal-blackmamba.png'
+        }
+      ]
+    };
+  },
+  getters: {
+    animals(state) {
+      return state.animals;
+    }
+  }
+};
