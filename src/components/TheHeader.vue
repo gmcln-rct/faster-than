f8df045c1d25634eea1ed42c47002e9ce9b7de7c
gmcln-rct <producersama@gmail.com>
2021-02-17 18:53:06 -0500

    Work on subheader

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -10,13 +10,17 @@
 </template>
 
 <style scoped>
+@import url('https://use.typekit.net/art4tjq.css');
+
 header {
   display: flex;
   justify-content: center;
+  flex-direction: column;
   width: 100%;
+  align-items: center;
+
   /* height: 5rem; */
   /* background-color: #3d008d; */
-  align-items: center;
 }
 
 header a {
@@ -35,10 +39,19 @@ a.router-link-active {
 
 h1 {
   margin: 0;
+  padding: 2vmin 0;
+  font-family: ivypresto-headline, serif;
+  font-weight: 700;
+  font-style: italic;
   font-size: calc(48px + (56 - 48) * ((100vw - 300px) / (1600 - 300)));
 }
 
 h2 {
+  font-family: adobe-caslon-pro, serif;
+
+  font-weight: 400;
+
+  font-style: normal;
   font-size: calc(14px + (32 - 14) * ((100vw - 300px) / (1600 - 300)));
 }
 
@@ -53,11 +66,11 @@ h1 a.router-link-active {
   border-color: transparent;
 }
 
-header nav {
+/* header nav {
   width: 90%;
   margin: auto;
   display: flex;
   justify-content: center;
   align-items: center;
-}
+} */
 </style>
