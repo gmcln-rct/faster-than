00a5dbb5b87846acb9ee17af75369702c55dce93
gmcln-rct <producersama@gmail.com>
2021-02-20 21:02:42 -0500

    Work on animal data

--- src/components/TheSelector.vue
+++ src/components/TheSelector.vue
@@ -34,61 +34,13 @@
 </template>
 
 <script>
+import animals from './data/animals.js';
 export default {
   data() {
     return {
       animal1: 'noanimal',
       animal2: 'noanimal',
-      animals: [
-        {
-          id: 'noanimal',
-          commonName: 'Choose Animal',
-          scientificName: '',
-          speed: '',
-          siteLink: '',
-          img: ''
-        },
-        {
-          id: 'animal001',
-          commonName: 'African wild dog',
-          scientificName: 'Lycaon pictus',
-          speed: 45,
-          siteLink: 'https://en.wikipedia.org/wiki/African_wild_dog',
-          img: 'https://faster-than.s3.amazonaws.com/animal-wilddog.png'
-        },
-        {
-          id: 'animal002',
-          commonName: "Anna's hummingbird",
-          scientificName: 'Calypte anna',
-          speed: 61.06,
-          siteLink: 'https://en.wikipedia.org/wiki/Anna%27s_hummingbird',
-          img: 'https://faster-than.s3.amazonaws.com/animal-annahummingbird.png'
-        },
-        {
-          id: 'animal003',
-          commonName: 'Atlantic bluefin tuna',
-          scientificName: 'Thunnus thynnus',
-          speed: 43.51,
-          siteLink: 'https://en.wikipedia.org/wiki/Atlantic_bluefin_tuna',
-          img: 'https://faster-than.s3.amazonaws.com/animal-bluefin.png'
-        },
-        {
-          id: 'animal004',
-          commonName: 'Bison',
-          scientificName: 'Bison',
-          speed: 35,
-          siteLink: 'https://en.wikipedia.org/wiki/Bison',
-          img: 'https://faster-than.s3.amazonaws.com/animal-annahummingbird.png'
-        },
-        {
-          id: 'animal005',
-          commonName: 'Black mamba snake',
-          scientificName: 'Dendroaspis polylepis',
-          speed: 12.9,
-          siteLink: 'https://en.wikipedia.org/wiki/Black_mamba',
-          img: 'https://faster-than.s3.amazonaws.com/animal-blackmamba.png'
-        }
-      ]
+      animals: animals
     };
   },
   computed: {
