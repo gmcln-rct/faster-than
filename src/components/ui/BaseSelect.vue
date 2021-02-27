372f713f24471a90fe7ab4b585bc08f058ba48f4
gmcln-rct <producersama@gmail.com>
2021-02-27 17:32:07 -0500

    Connect BaseSelect

--- src/components/ui/BaseSelect.vue
+++ src/components/ui/BaseSelect.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    <select :value="value" @input="updateValue" v-bind="$attrs"> </select>
+    <select :value="value" @input="updateValue" v-bind="$attrs">
+      <option v-for="option in options" :key="option">{{ option }}</option>
+    </select>
   </div>
 </template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    options: {
+      type: Array,
+      require: true
+    }
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import store from './store/index';
 import App from './App.vue';
 
 import BaseCard from './components/ui/BaseCard.vue';
+import BaseSelect from './components/ui/BaseSelect.vue';
 
 const app = createApp(App);
 
 app.use(store);
 
 app.component('base-card', BaseCard);
+app.component('base-select', BaseSelect);
 
 app.mount('#app');
