a8bbc1a5fce3c90b2ec407755858550c2add956e
gmcln-rct <producersama@gmail.com>
2021-03-01 20:31:09 -0500

    Work on expanding individual animal info

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -4,6 +4,23 @@
     <p>Copyright @2021 Glenn McClanan.</p>
   </footer>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      link1Text: 'Texas',
+      link1:
+        'https://tpwd.texas.gov/publications/nonpwdpubs/young_naturalist/animals/animal_speeds/',
+      link2Text: 'Hawaii',
+      link2: 'http://www.pbrc.hawaii.edu/~petra/animal_olympians.html',
+      link3Text: 'Natural HIstory Magazine',
+
+      link3:
+        'https://www.factmonster.com/math-science/biology/plants-animals/speed-of-animals'
+    };
+  }
+};
+</script>
 
 <style scoped>
 @import url('https://use.typekit.net/art4tjq.css');
