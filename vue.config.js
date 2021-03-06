97de38485e16af45e00b847c3e3ce345f2d95e1d
gmcln-rct <producersama@gmail.com>
2021-03-05 19:12:39 -0500

    Add deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production' ? '/vue-faster/' : '/'
-// };
 module.exports = {
-  publicPath: '/vue-faster/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-faster/' : '/'
 };
+// module.exports = {
+//   publicPath: '/vue-faster/'
+// };
