4e04f55906a5941a0a3296d5ad27bb1e105ec563
gmcln-rct <producersama@gmail.com>
2021-03-07 10:06:03 -0500

    Update deploy doc

--- deploy.sh
+++ deploy.sh
@@ -22,4 +22,7 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
+git push -f git@github.com:gmcln-rct/vue-faster.git main:gh-pages
+
+
 cd -
