c0f096eb64d405cd0c6275963537b4f8dfdbfbec
gmcln-rct <producersama@gmail.com>
2021-03-17 18:36:16 -0400

    Work on Quiz

--- src/pages/QuizPage.vue
+++ src/pages/QuizPage.vue
@@ -2,13 +2,16 @@
   <section class="quiz">
     <h2>Animal Speed Quiz</h2>
     <p>Which animal is faster?</p>
+    <p>{{ animals[0] }}</p>
   </section>
 </template>
 
 <script>
+import { animals } from '../components/data/animals.js';
 export default {
   data() {
     return {
+      animals: animals,
       currentRound: 0,
       playerWin: false,
       animalPairs: []
