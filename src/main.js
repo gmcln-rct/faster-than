4af595fde44839c1fa38560afad54ea8f350a134
gmcln-rct <producersama@gmail.com>
2021-04-01 18:55:42 -0400

    Work on meta tags

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue';
-import {
-  createMetaManager,
-  defaultConfig,
-  resolveOption,
-  useMeta
-} from 'vue-meta';
+// import {
+//   createMetaManager,
+//   defaultConfig,
+//   resolveOption,
+//   useMeta
+// } from 'vue-meta';
 
 import App from './App.vue';
 import router from './router.js';
@@ -14,38 +14,9 @@ import BaseSelect from './components/ui/BaseSelect.vue';
 
 const app = createApp(App);
 
-const decisionMaker5000000 = resolveOption((prevValue, context) => {
-  const { uid = 0 } = context.vm || {};
-  if (!prevValue || prevValue < uid) {
-    return uid;
-  }
-});
-
-const metaManager = createMetaManager(
-  {
-    ...defaultConfig,
-    esi: {
-      group: true,
-      namespaced: true,
-      attributes: ['src', 'test', 'text']
-    }
-  },
-  decisionMaker5000000
-);
-
-useMeta(
-  {
-    og: {
-      something: 'test'
-    }
-  },
-  metaManager
-);
-
 app.component('base-card', BaseCard);
 app.component('base-select', BaseSelect);
 
 app.use(router);
-app.use(metaManager);
 
 app.mount('#app');
